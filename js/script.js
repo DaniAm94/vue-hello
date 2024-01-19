357db1bf251ca7bb86362497b34462311ce2be7b
DaniAm94 <dani.amico94@gmail.com>
2024-01-19 15:52:03 +0100

    completed exercise plus bonus

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+// Per controllare di aver correttamente collegato lo script di vue
 console.log(Vue);
+
+// Procedura per utilizzare vue
+const { createApp } = Vue;
+
+const app = createApp({
+    data() {
+        return {
+            message: 'Messaggio a caso in un h1.',
+            img: 'img/R.jpeg',
+            alternativeText: 'testo alternativo',
+        }
+    }
+});
+app.mount('#root');
